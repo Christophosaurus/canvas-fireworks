9f84316559d278b582a2173acf795205efb063e5
Christophosaurus <christophosaurus@gmail.com>
2021-02-26 00:28:54 +0000

    Adding return types to util functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
-export function randomIntFromRange(min: number, max: number) {
+export function randomIntFromRange(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function randomColor(colors: string[]) {
+export function randomColor(colors: string[]): string {
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
-export function distance(x1: number, y1: number, x2: number, y2: number) {
+export function distance(x1: number, y1: number, x2: number, y2: number): number {
     const xDist = x2 - x1;
     const yDist = y2 - y1;
 
